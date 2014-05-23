2a6484da2b477fcae428424c264ead1f2adc09c0
Kohei Izumi <kohei.izumi@gmail.com>
2014-05-23 19:14:55 +0900

    シリアル通信で指定された文字列を受け取ると、ledをon, off

--- mbed-serial/mbed_serial.cpp
+++ mbed-serial/mbed_serial.cpp
@@ -1,8 +1,7 @@
 #include "mbed.h"
 
-#define READBUFFERSIZE (32)
-
 Serial pc(USBTX, USBRX);
+DigitalOut led1(LED1);
 
 int main()
 {
@@ -12,5 +11,11 @@ int main()
             pc.scanf("%s", &buffer);
         }
         pc.printf("%s\n", buffer);
+        if(!strcmp(buffer, "on")){
+            led1 = 1;
+        }
+        if(!strcmp(buffer, "off")){
+            led1 = 0;
+        }
     }
 }
