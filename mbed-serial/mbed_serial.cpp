702146e17f041a5dddfce2bb09327559e9639d7c
Kohei Izumi <kohei.izumi@gmail.com>
2014-05-23 18:40:06 +0900

    前回のプログラムみたいに複雑でなくても送受信出来るため、そちらに変更

--- mbed-serial/mbed_serial.cpp
+++ mbed-serial/mbed_serial.cpp
@@ -2,61 +2,15 @@
 
 #define READBUFFERSIZE (32)
 
-Serial g_serial(USBTX, USBRX);
-
-int ReadLineString( Serial& serial,
-                    char szReadBuffer[], const int ciReadBufferSize, int& riIndexChar,
-                    char szLineString[], const int ciLineStringSize )
-{
-    while( 1 )
-    {
-        if( !serial.readable() )
-        {
-            break;
-        }
-        char c = serial.getc();
-        if( '\r' == c  )
-        {
-            szReadBuffer[riIndexChar] = '\0';
-            strncpy( szLineString, szReadBuffer, ciLineStringSize - 1 );
-            szLineString[ciLineStringSize - 1] = '\0';
-            riIndexChar = 0;
-            return 1;
-        }
-        else if( '\n' == c )
-        {
-            ;
-        }
-        else
-        {
-            if( (ciReadBufferSize - 1) > riIndexChar )
-            {
-                szReadBuffer[riIndexChar] = c;
-                riIndexChar++;
-            }
-        }
-    }
-
-    return 0;
-}
+Serial pc(USBTX, USBRX);
 
 int main()
 {
-    // setup
-    g_serial.baud(9600);
-    wait(0.001);
-    
-    // loop
-    char szReadBuffer[READBUFFERSIZE] = "";
-    int iIndexChar = 0;
-    while(1)
-    {
-        char szLineString[READBUFFERSIZE];
-        if( !ReadLineString( g_serial, 
-                             szReadBuffer, READBUFFERSIZE, iIndexChar,
-                             szLineString, READBUFFERSIZE ) )
-        {
-            continue;
+    char buffer[20];
+    while(1){
+        if (pc.readable()){
+            pc.scanf("%s", &buffer);
         }
+        pc.printf("%s\n", buffer);
     }
 }
